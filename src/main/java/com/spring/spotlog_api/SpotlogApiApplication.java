7101d1aed98caf2a0faf95023d5715612852bfde
jeongii <hyenj629@naver.com>
2026-06-16 12:06:46 +0900

    feat: initialize spring boot project

---
+++ src/main/java/com/spring/spotlog_api/SpotlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spotlog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/spotlog_api/SpotlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spotlog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
